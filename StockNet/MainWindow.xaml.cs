11bb83c5ce75b9653a5d79a1159a801d64648749
unknown <lingonberry99@gmail.com>
2015-03-22 22:57:07 -0500

    Init

---
+++ StockNet/MainWindow.xaml.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StockNet
+{
+    /* Stock predictor
+     * File location: http://www.google.com/finance/getprices?i=[freq]&p=[days]d&f=d,o,h,l,c,v&df=cpct&q=[name]
+     * Columns: DATE,CLOSE,HIGH,LOW,OPEN,VOLUME
+     */
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Neural Network
+        NeuralNetwork[] networks;
+        double[] errors;
+        const int INPUTNUM = 15;
+        const int HIDDENNUM = 10;
+        const int OUTPUTNUM = 1;
+
+        // Genetic Algorithm
+        const int POPULATION = 200;
+        const int GEN = 1000;
+        const int ELITE = 10;
+        const double MUTATION = 0.01;
+
+        // Data
+        double[] data;
+        const int TICKS = 391;
+        const int DAYS = 10;
+
+        const int NORMALIZE = 200;
+        Random random = new Random();
+
+        private void StartTrain(object sender, RoutedEventArgs e)
+        {
+            // Initialize networks
+            networks = new NeuralNetwork[POPULATION];
+            for (int i = 0; i < POPULATION; i++)
+            {
+                networks[i] = new NeuralNetwork(INPUTNUM, HIDDENNUM, OUTPUTNUM, random);
+            }
+            errors = new double[POPULATION];
+
+            // Load data
+            StreamReader sr = new StreamReader("C:/Users/costco0371/Documents/Visual Studio 2013/Projects/StockNet/StockNet/data.txt");
+            string text = sr.ReadToEnd();
+            sr.Close();
+
+            // Parse data
+            data = new double[DAYS * TICKS];
+            string[] lines = text.Split('\n');
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] line = lines[i].Split(',');
+                data[i] = Convert.ToDouble(line[1]);
+            }
+
+            // Begin training
+            for (int gen = 0; gen < GEN; gen++)
+            {
+                // Pick a random day to sample
+                int day = random.Next(0, DAYS);
+
+                // Test each network in the population
+                for (int i = 0; i < POPULATION; i++)
+                {
+                    errors[i] = 0;
+                    // Test each window of size INPUTNUM
+                    for (int j = INPUTNUM; j < TICKS; j++)
+                    {
+                        // Fill the input values
+                        for (int k = j - INPUTNUM; k < j; k++)
+                            networks[i].inputValues[k - j + INPUTNUM] = data[day * TICKS + k] / NORMALIZE;
+
+                        // Forward propagate
+                        networks[i].FeedForward();
+
+                        // Get error
+                        double a = networks[i].outputValues[0] * NORMALIZE;
+                        double b = data[day + j];
+                        errors[i] += Math.Abs(networks[i].outputValues[0] * NORMALIZE - data[day * TICKS + j]) / data[day * TICKS + j];
+                    }
+                    networks[i].fitness = errors[i];
+                }
+
+                // Genetic Algorithm
+                GeneticAlgorithm();
+
+                string write = "gen: " + gen + ", error: " + errors.Min();
+                Console.WriteLine(write);
+            }
+        }
+
+        public void GeneticAlgorithm()
+        {
+            Array.Sort(errors, networks);
+            //Array.Reverse(networks);
+
+            for (int i = ELITE; i < POPULATION; i++)
+            {
+                // Pick two parents
+                int parent1 = random.Next(0, ELITE);
+                int parent2 = random.Next(0, ELITE);
+
+                // Pick two splice points
+                int geneNum = HIDDENNUM * (INPUTNUM + 1) + OUTPUTNUM * (HIDDENNUM + 1);
+                int splice1 = random.Next(0, (int)(geneNum / 2));
+                int splice2 = splice1 + (int)(geneNum / 2);
+
+                // Get parent genes
+                double[] genes1 = networks[parent1].FlattenWeights();
+                double[] genes2 = networks[parent2].FlattenWeights();
+
+                // Splice genes
+                double[] cgenes = new double[geneNum];
+                double gene;
+                for (int j = 0; j < geneNum; j++)
+                {
+                    gene = random.NextDouble();
+                    if (gene < MUTATION)
+                        cgenes[j] = gene;
+                    else if (j < splice1 || j >= splice2)
+                        cgenes[j] = genes1[j];
+                    else
+                        cgenes[j] = genes2[j];
+                }
+
+                // Inflate genes
+                networks[i].InflateGenes(cgenes);
+            }
+        }
+    }
+}
---
+++ StockNet/NeuralNetwork.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockNet
+{
+    class NeuralNetwork
+    {
+        // Number of neurons
+        private int inputNum;
+        private int hiddenNum;
+        private int outputNum;
+
+        // Weights
+        public double[][] hiddenWeights;
+        public double[] hiddenBiases;
+        public double[][] outputWeights;
+        public double[] outputBiases;
+
+        // Neuron values
+        public double[] inputValues;
+        public double[] hiddenValues;
+        public double[] outputValues;
+
+        public double fitness;
+
+        private Random random;
+
+        public NeuralNetwork(int inputNum, int hiddenNum, int outputNum, Random random)
+        {
+            this.inputNum = inputNum;
+            this.hiddenNum = hiddenNum;
+            this.outputNum = outputNum;
+            this.random = random;
+
+            // Initialize neuron value arrays
+            inputValues = new double[inputNum];
+            hiddenValues = new double[hiddenNum];
+            outputValues = new double[outputNum];
+
+            // Initialize weight arrays
+            hiddenWeights = new double[hiddenNum][];
+            hiddenBiases = new double[hiddenNum];
+            outputWeights = new double[outputNum][];
+            outputBiases = new double[outputNum];
+
+            // Initialize random weights
+            for (int i = 0; i < hiddenNum; i++)
+            {
+                hiddenWeights[i] = new double[inputNum];
+                for (int j = 0; j < inputNum; j++)
+                {
+                    hiddenWeights[i][j] = random.NextDouble();
+                }
+                hiddenBiases[i] = random.NextDouble();
+            }
+            for (int i = 0; i < outputNum; i++)
+            {
+                outputWeights[i] = new double[hiddenNum];
+                for (int j = 0; j < hiddenNum; j++)
+                {
+                    outputWeights[i][j] = random.NextDouble();
+                }
+                outputBiases[i] = random.NextDouble();
+            }
+        }
+
+        public void FeedForward()
+        {
+            double sum;
+
+            // Compute hidden layer
+            for (int i = 0; i < hiddenNum; i++)
+            {
+                sum = 0;
+                for (int j = 0; j < inputNum; j++)
+                {
+                    sum += inputValues[j] * hiddenWeights[i][j];
+                }
+                hiddenValues[i] = 1.0 / (1.0 + Math.Exp(-(sum - hiddenBiases[i])));
+            }
+
+            // Compute output layer
+            for (int i = 0; i < outputNum; i++)
+            {
+                sum = 0;
+                for (int j = 0; j < hiddenNum; j++)
+                {
+                    sum += hiddenValues[j] * outputWeights[i][j];
+                }
+                outputValues[i] = 1.0 / (1.0 + Math.Exp(-(sum - outputBiases[i])));
+            }
+        }
+
+        public double[] FlattenWeights()
+        {
+            double[] weights = new double[hiddenNum * (inputNum + 1) + outputNum * (hiddenNum + 1)];
+            int c = 0;
+
+            for (int i = 0; i < hiddenNum; i++)
+            {
+                for (int j = 0; j < inputNum; j++)
+                {
+                    weights[c] = hiddenWeights[i][j];
+                    c++;
+                }
+                weights[c] = hiddenBiases[i];
+                c++;
+            }
+            for (int i = 0; i < outputNum; i++)
+            {
+                for (int j = 0; j < hiddenNum; j++)
+                {
+                    weights[c] = outputWeights[i][j];
+                    c++;
+                }
+                weights[c] = outputBiases[i];
+                c++;
+            }
+
+            return weights;
+        }
+
+        public void InflateGenes(double[] weights)
+        {
+            int c = 0;
+
+            for (int i = 0; i < hiddenNum; i++)
+            {
+                for (int j = 0; j < inputNum; j++)
+                {
+                    hiddenWeights[i][j] = weights[c];
+                    c++;
+                }
+                hiddenBiases[i] = weights[c];
+                c++;
+            }
+            for (int i = 0; i < outputNum; i++)
+            {
+                for (int j = 0; j < hiddenNum; j++)
+                {
+                    outputWeights[i][j] = weights[c];
+                    c++;
+                }
+                outputBiases[i] = weights[c];
+                c++;
+            }
+        }
+
+    }
+}
